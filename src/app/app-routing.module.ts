a9cd01b2abf539797d52c405b25beef01c78462f
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-30 18:07:43 -0500

    create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { UsComponent } from './components/us/us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'us', component: UsComponent },
+  // { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { UsComponent } from './components/us/us.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactComponent,
+    HomeComponent,
+    ProductComponent,
+    UsComponent,
   ],
   imports: [
     BrowserModule,
