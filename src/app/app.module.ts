5587648bd16ba4869a6836b88ea02e6355e66562
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-30 23:45:35 -0500

    before test login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
@@ -12,12 +16,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     AppComponent,
     LoginComponent,
     NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { LoginService } from 'src/app/services/login.service';
+
+interface User {
+  username: String;
+  password: String;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private authGuard: AuthGuard) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    if (this.user.username != "" && this.user.password != "") {
+      console.log("correct");
+      this.authGuard.canActivate()
+    } else {
+      console.log("incorrect");
+    }
+    return false
   }
 
 }
